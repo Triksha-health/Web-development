c8037419085ee22bc69aadf663fcdc30fbe0447b
Sourabh dharra <s.dharra88@gamil.com>
2025-06-11 10:44:53 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import Layout from './components/layout/Layout';
+import HomePage from './components/home/Hero';
+
+
+function App() {
+  useEffect(() => {
+    document.title = 'Triksha - AI-Powered Health Wearable';
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
