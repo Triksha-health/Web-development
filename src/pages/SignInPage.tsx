347d49c8f28cb9148cc44297f36d7d95990a250b
Sourabh dharra <s.dharra88@gamil.com>
2025-06-12 03:41:51 +0530

    add signIn page

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -26,11 +26,7 @@ function SignInPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        {/* <div className="flex justify-center">
-              <img src={logo} alt=""  className='w-20 h-10'/>
-           
-          
-        </div> */}
+        
         <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
           Sign in to your account
         </h2>
