45aa23c9e8e63bc7364d1965721d10a2e344856c
Sudhamayee <sudhamayeej07@gmail.com>
2025-06-19 23:10:35 +0530

    Add preorder API and update server config

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config(); // 👈 should be at the very top
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const preOrderRoutes = require("./routes/preOrderRoutes");
 
 dotenv.config();
 connectDB();
@@ -10,6 +12,8 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use("/api/preorder", preOrderRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
