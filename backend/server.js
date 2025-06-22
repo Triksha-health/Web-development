7aee247a118bc06377a856bf5eee63cb419f9e14
Sudhamayee <sudhamayeej07@gmail.com>
2025-06-22 17:09:22 +0530

    Added apis successfully

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const preOrderRoutes = require("./routes/preOrderRoutes");
+const contactRoutes = require('./routes/contactRoutes');
+const faqRoutes = require('./routes/faqRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const newsletterRoutes = require('./routes/newsletterRoutes');
+const systemRoutes = require("./routes/systemRoutes");
 
 dotenv.config();
 connectDB();
@@ -13,6 +18,11 @@ const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use("/api/preorder", preOrderRoutes);
+app.use('/api', contactRoutes);
+app.use('/api', faqRoutes);
+app.use('/api/blogs', blogRoutes);
+app.use('/api/newsletter', newsletterRoutes);
+app.use("/api/system", systemRoutes);
 
 
 const PORT = process.env.PORT || 5000;
