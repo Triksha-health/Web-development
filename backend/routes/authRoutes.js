ea7701ee5ebfd652b297a036422b223bd96a3ca3
Yashwanth <Triksha.team@gmail.com>
2025-06-28 23:09:22 +0530

    Update authRoutes.js
    added test route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
-
+router.get('/', (req, res) => {
+  res.send('✅ Auth API is live!');
+});
 router.post('/register', register);
 router.post('/login', login);
 
