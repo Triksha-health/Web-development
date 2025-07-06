26f75cc1bbf32d8ff05339d352b57999e47766fa
7007259Ankur <ankurkumar7753@gmail.com>
2025-07-06 16:52:42 +0530

    FAQ with post and get method

--- backend/routes/faqRoutes.js
+++ backend/routes/faqRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getFAQ, updateFAQ } = require('../controllers/faqController');
+const { getFAQ, updateFAQ, createFAQ } = require('../controllers/faqController');
+
 
 router.get('/faq', getFAQ);
 router.put('/faq/:id', updateFAQ);
+router.post('/faq', createFAQ);   // ✅ this is required!
+
 
 module.exports = router;
