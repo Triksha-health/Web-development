169910c1f8131461790c734b029c49e6a6cc5d86
Yashwanth <Triksha.team@gmail.com>
2026-02-14 20:15:33 +0530

    changed the team section totally

--- frontend/src/pages/TeamsPage.tsx
+++ frontend/src/pages/TeamsPage.tsx
@@ -4,19 +4,8 @@ import { useEffect } from "react";
 import { Check } from "lucide-react";
 import familylaptop from "../public/familylaptop.jpg";
 import TeamSection from "../components/ui/TeamSection";
-import rohan from "../public/rohan.jpg";
-import rajat from "../public/rajat.jpg";
-import ankur from "../public/ankur.jpg";
-import shubranvi from "../public/shubranvi.jpg";
-import sudhamayye from "../public/sudhamayye.jpg";
-import swaraj from "../public/swarajzore.jpg";
-import ayush from "../public/hecker.jpg";
-import vaish from "../public/vaishnavireddy.jpg";
-import vrinda from "../public/vrinda.jpg";
-import sachin from "../public/sachin.jpg";
-import saurabh from "../public/saurabh.jpg";
 import yash from "../public/yash.jpg";
-import ujjwal from "../public/ujjwal.jpg";
+import pulkishore from "../public/puli kishore1.jpg";
 import SectionHeading from "../components/ui/SectionHeading";
 import { Link } from "react-router-dom";
 
@@ -35,111 +24,12 @@ const TeamsPage = () => {
       github: "https://github.com/janedoe",
     },
     {
-      name: "Ujjwal Rathore",
-      role: "AI Engineer",
-      bio: "IIIT Guwahati student who specialises in AI and machine learning. Passionate about using technology to improve healthcare outcomes.",
-      image: ujjwal,
-      github: "https://github.com/ujjwal19rathore218",
-      linkedin: "https://www.linkedin.com/in/ujjwal-rathore-40004a25b",
-      twitter: "",
-    },
-    {
-      name: "Swaraj Zore",
-      role: "AI Engineer",
-      bio: "Fueling innovation with ML brilliance—Athatva College of Engineering's data-driven trailblazer shaping smarter solutions.",
-      image: swaraj,
-      github: "https://github.com/SwarajZore",
-      twitter: "",
-      linkedin: "https://www.linkedin.com/in/swaraj-zore-48957a2ba",
-    },
-    {
-      name: "Sachin Kumar",
-      role: "AI Engineer",
-      bio: "Shaping tomorrow with creativity and code—IIT Varanasi’s GenAI enthusiast driving the frontier of intelligent innovation.",
-      image: sachin,
-      github: "https://github.com/KumarSachin-DS",
-      linkedin: "https://www.linkedin.com/in/sachin-kumar-22188b1ba",
-      twitter: "",
-    },
-    {
-      name: "Shubhranvi Kapare",
-      role: "AI Engineer",
-      bio: "Driving intelligent breakthroughs—AI Engineer from Pimpri Chinchwad College of Engineering, crafting the future with smart tech.",
-      image: shubranvi,
-      github: "https://www.linkedin.com/in/shubhranvi-kapare-7463b2291",
-      linkedin: "https://www.linkedin.com/in/shubhranvi-kapare-7463b2291",
-      twitter: "",
-    },
-    {
-      name: "Rajat Sharma",
-      role: "Computer Vision Engineer",
-      bio: "Innovating at every layer—MBM Jodhpur’s multifaceted techie blending AI, code, and creative design into future-ready solutions.",
-      image: rajat,
-      github: "https://github.com/Rajat-Shrma",
-      linkedin: "https://www.linkedin.com/in/rajat-749384290",
-      twitter: "",
-    },
-    {
-      name: "Ayush",
-      role: "Computer Vision Engineer",
-      bio: "Merging vision with intelligence—IIIT Guwahati's ML & CV engineer crafting next-gen tech that sees and understands the world.",
-      image: ayush,
-      github: "https://github.com/Ayush21CSR",
-      linkedin: "https://www.linkedin.com/in/ayush-singh-89b620261/",
-      twitter: "",
-    },
-    {
-      name: "Rohan Lohiya",
-      role: "Fullstack Developer",
-      bio: "IIIT Guwahati student who specializes in fullstack development. Passionate about building user-friendly real-time applications.",
-      image: rohan,
-      github: "https://github.com/Rohan-Lohiya",
-      linkedin: "https://www.linkedin.com/in/rohan-lohiya-b16518172/",
-      twitter: "",
-    },
-    {
-      name: "Sourabh Dharra",
-      role: "Fullstack Developer",
-      bio: "RGIPT student specializing in full-stack development, passionate about building efficient, scalable solutions for real-time applications.",
-      image: saurabh,
-      github: "https://github.com/SOURABH-05",
-      linkedin: "https://www.linkedin.com/in/sourabh-dharra-707294288/",
-      twitter: "",
-    },
-    {
-      name: "Ankur Gupta",
-      role: "Fullstack Developer | Cloud Engineer",
-      bio: "Architecting seamless systems—Backend Node.js & Express.js developer and Cloud Engineer from IIIT Guwahati, scaling apps with AWS and Azure.",
-      image: ankur,
-      github: "https://github.com/7007259Ankur",
-      linkedin: "https://www.linkedin.com/in/ankur-gupta-15221a24b",
-      twitter: "",
-    },
-    {
-      name: "Sudhamayee",
-      role: "Fullstack Developer",
-      bio: "Crafting seamless digital experiences—CBIT’s full stack & backend developer building robust, scalable solutions from front to core.",
-      image: sudhamayye,
-      github: "https://github.com/sudhaamayee",
-      linkedin: "https://www.linkedin.com/in/sudhamayee-j-456974361",
-      twitter: "",
-    },
-    {
-      name: "Vaishnavi",
-      role: "Data Engineer",
-      bio: "Transforming raw data into powerful insights—CBIT's data engineer turning numbers into knowledge for smarter decisions.",
-      image: vaish,
-      github: "https://github.com/vaishnavi-100106/",
-      linkedin: "https://www.linkedin.com/in/vaishnavi-r-a840a1301",
-      twitter: "",
-    },
-    {
-      name: "Vrinda singh",
-      role: "Data Engineer",
-      bio: "Empowering insights through data—CBIT's data engineer building pipelines and systems that turn complexity into clarity.",
-      image: vrinda,
-      github: "https://github.com/vrinda07-ui",
-      linkedin: "https://www.linkedin.com/in/vrinda-singh-aa135b29a",
+      name: "Dr.Puli Kishore Kumar",
+      role: "Advisor",
+      bio: "Assistant Professor at NIT Andhra Pradesh and signal processing researcher with a PhD from NIT Warangal, focused on ultra-wideband radar imaging, compressive sensing.",
+      image: pulkishore ,
+      github: "",
+      linkedin: "https://www.linkedin.com/in/dr-puli-kishore-kumar-70948a48",
       twitter: "",
     },
   ];
